57fad964a2a67ee9e0f3c45da716b9b05250a7ee
wilcesde <wilcascesde@gmail.com>
2020-09-09 19:43:14 -0500

    add operArit file

---
+++ exampleOne/js/operArit.js
@@ -0,0 +1,32 @@
+function sumar() {
+    //Recibir los datos
+    let numeroU = parseInt(document.getElementById("nUno").value)
+    let numeroD = parseInt(document.getElementById("nDos").value)
+
+    //Procesar los datos
+    let resultado = parseInt(numeroU + numeroD)
+
+    //Entregar los resultados
+    document.getElementById("resulOperacion").innerHTML = resultado
+    
+}
+
+function multiplicar() {
+    //Recibir los datos
+    let numeroU = parseInt(document.getElementById("nUno").value)
+    let numeroD = parseInt(document.getElementById("nDos").value)
+
+    //Procesar los datos
+    let resultado = parseInt(numeroU * numeroD)
+
+    //Entregar los resultados
+    document.getElementById("resulOperacion").innerHTML = resultado
+    
+}
+
+//Calcular area triangulo
+
+
+//Calcular area circulo
+
+    
