50fdf702c597b7b5b20002869c878c022a5b95cf
wilcesde <wilcascesde@gmail.com>
2020-09-16 20:35:41 -0500

    update if, case, tipo flecha

--- exampleOne/js/eDecision.js
+++ exampleOne/js/eDecision.js
@@ -40,19 +40,78 @@ function mostrarMensajeDos() {
     // }
 
     //If con operador logico AND(&&)
-    if (edadP <= parametroEdad && estaturaP >= parametroEstatura && pesoP == parametroPeso) {
+    /*if (edadP <= parametroEdad && estaturaP >= parametroEstatura && pesoP == parametroPeso) {
         mensajeDecision = "Apto para realizar el deporte"
     } else {
         mensajeDecision = "No Apto para realizar el deporte"
-    }
+    } */
 
     //If con operador logico OR(||)
-    if (edadP <= parametroEdad || estaturaP >= parametroEstatura || pesoP == parametroPeso) {
+    /*if (edadP <= parametroEdad || estaturaP >= parametroEstatura || pesoP == parametroPeso) {
         mensajeDecision = "Apto para realizar el deporte"
     } else {
         mensajeDecision = "No Apto para realizar el deporte"
-    }
+    }*/
 
     alert(mensajeDecision)
+}
+
+function mostrarMensajeTres() {
+    let productName = document.getElementById('productName').value
+    let price = 0
+
+    const PRODUCT_ONE = 'A'
+    const PRODUCT_TWO = 'B'
+    const PRODUCT_THREE = 'C'
+    const MESSAGE = 'Invalid product name'
+
+    /*if (productName.toUpperCase() == PRODUCT_ONE) {
+        price = 2000
+    } else {
+        if (productName.toUpperCase() == PRODUCT_TWO) {
+            price = 2200
+        } else {
+            if (productName.toUpperCase() == PRODUCT_THREE) {
+                price = 1800
+            } else {
+                alert(MESSAGE.toUpperCase())
+            }
+        }
+    }
+
+    alert(price) */
+
+    switch (productName.toUpperCase()) {
+        case PRODUCT_ONE:
+            price = 20000
+            break;
+        case PRODUCT_TWO:
+            price = 22000
+            break;
+        case PRODUCT_THREE:
+            price = 18000
+            break;    
+        default:
+            alert(MESSAGE.toUpperCase())
+            break;
+    }
+
+    alert(Intl.NumberFormat().format(price))
+}
+
+//function prenderApagar(interruptor) 
+let prenderApagar = interruptor => {
+    let pic
+
+    /*if (interruptor == 0) {
+        alert('OFF')
+    } else {
+        alert('ON')
+    } */
+
+    let mensaje = (interruptor == 0) ? "OFF" : "ON"
+
+    alert(mensaje)
+    
 }
 
